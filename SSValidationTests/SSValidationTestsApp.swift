4fb5724ff1643f6c483111cfdd7c7b27aca35cef
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2022-03-19 17:37:42 +0100

    Initial Commit

---
+++ SSValidationTests/SSValidationTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSValidationTestsApp.swift
+//  SSValidationTests
+//
+//  Created by sebastianstaszczyk on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SSValidationTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
